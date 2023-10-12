8ef9b29d1e14f2a639aaf177885741fbeb10d4a4
Cheng Da <cheng.da@nasa.gov>
2023-10-12 15:01:00 -0400

    remove user-dependent modulese

---
+++ cylc/cpld_odas_0d25_both/etc/env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#umask 022
+#ulimit -s unlimited
+
+#module use -a ~sakella/modulefiles
+#module load python/17May2023
+#ml
+
+module use -a /home/mathomp4/modulefiles-SLES12
+module load python/ChengpyD/3.10
+ml
+
